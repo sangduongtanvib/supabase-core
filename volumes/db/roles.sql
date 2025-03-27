58d9656683f54db78846bff52cb1ce14f0ad1372
sang.duongtan <sang.duongtan@vib.com.vn>
2025-03-27 22:13:11 +0700

    Add SQL script to ensure supabase_admin role exists in the database

--- volumes/db/roles.sql
+++ volumes/db/roles.sql
@@ -6,3 +6,12 @@ ALTER USER pgbouncer WITH PASSWORD :'pgpass';
 ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
+
+-- Ensure supabase_admin exists
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'supabase_admin') THEN
+    CREATE ROLE supabase_admin WITH LOGIN PASSWORD :'pgpass' SUPERUSER;
+  END IF;
+END
+$$;
